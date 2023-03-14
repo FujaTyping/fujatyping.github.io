2eac8d0773372942fea7e10dbdd0f4c7c9063b47
Siraphop Sukchu <soraphopsukchu@gmail.com>
2023-03-14 22:06:31 +0700

    Update file

--- loader.js
+++ loader.js
@@ -1,8 +1,8 @@
 window.onload = function() {
     const Load = document.getElementById("loader")
     const App = document.getElementById("app")
-    const bg = document.getElementById("bg")
+    const Bg = document.getElementById("bg")
 
     Load.style.display = "none";
-    bg.style.display = "block";
+    Bg.style.display = "block";
 };
