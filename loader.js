d264989e343aa6b8d366c3e449b7778374da4fa4
Siraphop Sukchu <soraphopsukchu@gmail.com>
2023-03-14 11:31:10 +0700

    Add file (loader)

---
+++ loader.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    const Load = document.getElementById("loader")
+    const App = document.getElementById("app")
+    const bg = document.getElementById("bg")
+
+    Load.style.display = "none";
+    bg.style.display = "block";
+};
